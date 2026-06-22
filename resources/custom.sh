0a1c8ec220ccf8a616234f3de389136adb2ca445
LoRd_MuldeR <mulder2@gmx.de>
2026-06-22 22:39:49 +0200

    Added the required dependencies + force update of installed packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -3,3 +3,7 @@
 set -exu
 
 # Add your additional provisioning here for custom VM images.
+sed -i 's|r1beta5|master|g' /system/settings/package-repositories/Haiku
+pkgman refresh
+pkgman full-sync -y
+pkgman install -y rust_bin
